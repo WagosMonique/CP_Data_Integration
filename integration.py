d7d542934b3c390a2814e353ccebda7e8a8bc13b
WagosMonique <monique@wagos.com>
2025-12-30 16:25:08 -0400

    - more editing to input, - data needs to be structured better to be made into df, then made into a cleaned excel sheet w/ neccessary rows

--- integration.py
+++ integration.py
@@ -1,18 +1,28 @@
-from retriever import * 
+from retriever import *
 
 def main():
     dates=input("Please input the start date and end date in the format mm-dd-yyyy")
     dates=format_data(dates)  
-    print(dates)
+
     choice=input("1.A/R Adjustments\n2.Tickets\n")
 
+    if choice == '1':
+        get_ar(dates)
+    elif choice =='2':
+        get_tickets(dates)
 
 
 def format_data(dates):
     dates=dates.split(" ")
     return dates
 
+def get_ar(dates):
+    print("inside")
+    adj_query(dates)
+    return 
 
+def get_tickets(dates):
+    return 
 
 if __name__ =="__main__":
     main()
--- retriever.py
+++ retriever.py
@@ -1,8 +1,7 @@
 import pyodbc
 import pandas as pd
 
-cursor=''
-def main():
+def connection():
 
     cnxn= pyodbc.connect("Driver={SQL Server Native Client 11.0};" 
                         "Server=SUPPORT03;"
@@ -10,23 +9,28 @@ def main():
                         "uid=sa;pwd=CounterPoint8")
 
     cursor=cnxn.cursor()
-    adj_query()
     return cursor
 
 def adj_query(dates):
     # cursor.execute(f'SELECT * FROM AR_ADJ_HIST WHERE DOC_DAT >={dates[0]} AND DOC_DAT <={dates[1]}') 
-    cursor.execute(f'SELECT * FROM AR_ADJ_HIST WHERE DOC_DAT >={dates[0]} AND DOC_DAT <={dates[1]}')
+    print("hello")
+    cursor=connection()
     
+    cursor.execute("SELECT * FROM AR_ADJ_HIST")
+
     col=[]
     data=[]
-    for row in cursor.columns(table='AR_ADJ_HIST'):
-        col.append(row.column_name)
 
     for row in cursor:
         data.append(row)
+        
+    print(data)
+    for row in cursor.columns(table='AR_ADJ_HIST'):
+        col.append(row.column_name)
 
 
-    df_adj_hist=pd.DataFrame(columns=col,rows=data)
 
-if __name__ =="__main__":
-    main()
+    df_adj_hist=pd.DataFrame(data,columns=col)
+    print(df_adj_hist)
+    return df_adj_hist
+
