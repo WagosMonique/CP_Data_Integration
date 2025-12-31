82d7c030fda70ea201a92b39e33fb31d77b73da0
WagosMonique <monique@wagos.com>
2025-12-31 09:20:16 -0400

    + Able to extract Data from Database for A/R features + Creating excel sheet. > Formatting data

--- retriever.py
+++ retriever.py
@@ -1,7 +1,8 @@
 import pyodbc
 import pandas as pd
 
-def connection():
+# def connection():
+def main():
 
     cnxn= pyodbc.connect("Driver={SQL Server Native Client 11.0};" 
                         "Server=SUPPORT03;"
@@ -9,28 +10,30 @@ def connection():
                         "uid=sa;pwd=CounterPoint8")
 
     cursor=cnxn.cursor()
+    ar_df=adj_query(cursor)
+    ar_df.to_excel("export\\ar_data.xlsx")
+
     return cursor
 
-def adj_query(dates):
+def adj_query(cursor):
     # cursor.execute(f'SELECT * FROM AR_ADJ_HIST WHERE DOC_DAT >={dates[0]} AND DOC_DAT <={dates[1]}') 
-    print("hello")
-    cursor=connection()
-    
-    cursor.execute("SELECT * FROM AR_ADJ_HIST")
+   
+    cursor.execute("SELECT * FROM AR_ADJ_HIST")#get information from sql table
 
     col=[]
-    data=[]
+    data=[]# instantiating variables
 
-    for row in cursor:
-        data.append(row)
-        
-    print(data)
+    rows=cursor.fetchall()#getting all rows from table
+
+    for row in rows: 
+        data.append(list(row))
+    
     for row in cursor.columns(table='AR_ADJ_HIST'):
         col.append(row.column_name)
 
-
-
     df_adj_hist=pd.DataFrame(data,columns=col)
-    print(df_adj_hist)
+
     return df_adj_hist
 
+if __name__=='__main__':
+    main()
