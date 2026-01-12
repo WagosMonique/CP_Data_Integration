0b12d91d0c52b81cebd53ec3e773be7ac1184b39
WagosMonique <monique@wagos.com>
2026-01-12 10:56:31 -0400

    Ticket Line data, ticket data and ar_adjustments

--- retriever.py
+++ retriever.py
@@ -13,6 +13,12 @@ def main():
     ar_df=adj_query(cursor)
     ar_df.to_excel("export\\ar_data.xlsx")
 
+    tkt_df=tkt_hist(cursor)
+    tkt_df.to_excel("export\\tkt_data.xlsx")
+
+    tkt_lines_df=tkt_lines(cursor)
+    tkt_lines_df.to_excel("export\\tkt_lines_data.xlsx")
+
     return cursor
 
 def adj_query(cursor):   
@@ -55,6 +61,49 @@ def adj_query(cursor):
 
     df_adj_hist=df_adj_hist.drop(cols_to_drop,axis=1)
     return df_adj_hist
+
+def tkt_hist(cursor):
+    
+    
+    col=[]
+    data=[]# instantiating variables
+
+    
+    cursor.execute("SELECT * FROM PS_TKT_HIST")
+
+    rows=cursor.fetchall()#getting all rows from table
+
+    for row in rows: 
+        data.append(list(row))
+    
+    col = [column[0] for column in cursor.description]
+
+
+    df_tkt_hist=pd.DataFrame(data,columns=col)
+
+    return df_tkt_hist
+
+def tkt_lines(cursor):
+    
+    
+    col=[]
+    data=[]# instantiating variables
+
+    
+    cursor.execute("SELECT BUS_DAT, TKT_NO,ITEM_NO,QTY_SOLD,PRC_1,CALC_PRC,REG_PRC,PRC,EXT_COST,EXT_PRC,UNIT_RETL_VAL,GROSS_EXT_PRC,CALC_EXT_PRC FROM PS_TKT_HIST_LIN")
+
+    rows=cursor.fetchall()#getting all rows from table
+
+    for row in rows: 
+        data.append(list(row))
+    
+    col = [column[0] for column in cursor.description]
+
+
+    df_tkt_hist_lines=pd.DataFrame(data,columns=col)
+
+    return df_tkt_hist_lines
+
 
 if __name__=='__main__':
     main()
