ec1d597ae2e47e1fe564ee9bad010b4045d5f10b
moniquewagos <monique@wagos.com>
2026-01-21 16:50:13 -0400

    successfully got customer ids and item ids

--- integration.py
+++ integration.py
@@ -1,28 +1,81 @@
-from retriever import *
+from retriever import quote_import
+import xmlrpc.client
+import pandas as pd
+import pprint 
+
+url='https://wagosmonique-api-testing.odoo.com'#put your url name for your db here
+db='wagosmonique-api-testing-main-27639352'#can be found by doing echo $PGDATABASE in the shell
+
+username='monique@wagos.com'#the user email associated with your api key 
+password='fe235116d8a6fbac4f326f0eddad074527b0c213'#put your api key as the password
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))  # For auth
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))  # For data
 
 def main():
-    dates=input("Please input the start date and end date in the format mm-dd-yyyy")
-    dates=format_data(dates)  
+    
+        
+    uid= authentication()
+    print(uid)
+    data=quote_import()#ticket information
+    items=get_item_ids(data,uid)#dictionary of items on tickets
+    customers=get_cust_ids(data,uid)#dictionary of customers on tickets
+    post_tickets(items,customers,data)
+    
 
-    choice=input("1.A/R Adjustments\n2.Tickets\n")
 
-    if choice == '1':
-        get_ar(dates)
-    elif choice =='2':
-        get_tickets(dates)
+def authentication():
+
+    uid= common.authenticate(db, username, password, {})
+
+    if uid:
+        print("authentication successful")
+    else:
+        print("authentication failed ")
+
+    return uid
 
 
-def format_data(dates):
-    dates=dates.split(" ")
-    return dates
+def post_tickets(items,customers,tickets):
+    # data = models.execute_kw(db, uid, password, 'sale.order', 'create', [{}])
+    indicator = 0
+    for row in tickets.itertuples(index=False):
+         if indicator == 0: #indicates if we are on a new ticket 
+            if row.LINS != 0:#only take tickets with lines
+                indicator = int(row.LINS)#get the number of lines
+                header={'Reference':row.Order_Reference}
+                indicator -= 1
+         else:
+            indicator -= 1
 
-def get_ar(dates):
-    print("inside")
-    adj_query(dates)
+
     return 
 
-def get_tickets(dates):
-    return 
+
+def get_item_ids(data,uid):
+    products={}
+
+    for row in data.itertuples():
+        if row.Products not in products:#only unique products
+
+            item_id=models.execute_kw(db, uid, password, 'product.product', 'search_read', [[['name','=',row.Products]]], {'fields': ['id']})#can be used to get fields of a particular model
+            products[row.Products]=item_id[0]['id']
+        
+    return products
+
+
+def get_cust_ids(data,uid):
+    customers={}
+
+    for row in data.itertuples():
+        if row.Customer not in customers and row.Customer:#only unique products
+            customer_ids=models.execute_kw(db, uid, password, 'res.partner', 'search_read', [[['name','=',row.Customer]]], {'fields': ['id']})#can be used to get fields of a particular model
+            customers[row.Customer]=customer_ids[0]['id']
+
+    print(customers)
+        
+    return customers
+
+
 
 if __name__ =="__main__":
     main()
--- retriever.py
+++ retriever.py
@@ -1,116 +1,24 @@
 import pyodbc
 import pandas as pd
 
-# def connection(server):#this function performs the connection on a given server and returns a cursor.
+
+#retrieves information from CP DB 
+
+cnxn= pyodbc.connect("Driver={SQL Server Native Client 11.0};" 
+                    "Server=SUPPORT03;"
+                    "Database=TestGolf;"
+                    "uid=sa;pwd=CounterPoint8")
+
+cursor=cnxn.cursor()
+
 def main():
 
-    cnxn= pyodbc.connect("Driver={SQL Server Native Client 11.0};" 
-                        "Server=SUPPORT03;"
-                        "Database=TestGolf;"
-                        "uid=sa;pwd=CounterPoint8")
-
-    #the below is just for testing purposes and will be edited to integrate with the file integration
-    cursor=cnxn.cursor()
-    ar_df=adj_query(cursor)
-    ar_df.to_excel("export\\ar_data.xlsx")
-
-    tkt_df=tkt_hist(cursor)
-    tkt_df.to_excel("export\\tkt_data.xlsx")
-
-    tkt_lines_df=tkt_lines(cursor)
-    tkt_lines_df.to_excel("export\\tkt_lines_data.xlsx")
-
-    odoo_quotes=quote_import(cursor)
-    odoo_quotes.to_excel("export\\quotes_data.xlsx")
 
 
-    return cursor
-
-def adj_query(cursor):   
-    cursor.execute("SELECT c.NAM , a.* FROM AR_ADJ_HIST a JOIN AR_CUST c ON a.CUST_NO=c.CUST_NO")#get information from sql table
-    # make a join statetment and join the customer no.s to customer names, and just select the customer names 
-
-    col=[]
-    data=[]# instantiating variables
-
-    rows=cursor.fetchall()#getting all rows from table
-
-    for row in rows: 
-        data.append(list(row))
-    
-    col = [column[0] for column in cursor.description]
+    return
 
 
-    df_adj_hist=pd.DataFrame(data,columns=col)
-
-    #formatting data 
-
-    #drop event_no row
-    cols_to_drop=[
-        'EVENT_NO',
-        'DOC_TYP',
-        'BAT_ID',
-        'ENTRY_SEQ_NO',
-        'APPLY_TO_METH',
-        'APPLY_TO_DOC_TYP',
-        'DISC_DAT',
-        'SLS_REP',
-        'CUST_BAL_BEFORE',
-        'LST_MAINT_DT',
-        'LST_MAINT_USR_ID',
-        'LST_LCK_DT',
-        'ROW_TS',
-        'CUST_STR_ID',
-        'STR_ID'
-    ]#a bit unnesscessary will modify to select only columns needed 
-
-    df_adj_hist=df_adj_hist.drop(cols_to_drop,axis=1)
-    return df_adj_hist
-
-def tkt_hist(cursor):
-    
-    
-    col=[]
-    data=[]# instantiating variables
-
-    
-    cursor.execute("SELECT BUS_DAT,DOC_ID,TKT_NO,TKT_DT,TKT_TYP,CUST_NO,LINS,SUB_TOT,TOT_EXT_COST,TOT FROM PS_TKT_HIST")
-
-    rows=cursor.fetchall()#getting all rows from table
-
-    for row in rows: 
-        data.append(list(row))
-    
-    col = [column[0] for column in cursor.description]
-
-
-    df_tkt_hist=pd.DataFrame(data,columns=col)
-
-    return df_tkt_hist
-
-def tkt_lines(cursor):
-    
-    
-    col=[]
-    data=[]# instantiating variables
-
-    
-    cursor.execute("SELECT BUS_DAT, TKT_NO,ITEM_NO,QTY_SOLD,PRC_1,CALC_PRC,REG_PRC,PRC,EXT_COST,EXT_PRC,UNIT_RETL_VAL,GROSS_EXT_PRC,CALC_EXT_PRC FROM PS_TKT_HIST_LIN")
-
-    rows=cursor.fetchall()#getting all rows from table
-
-    for row in rows: 
-        data.append(list(row))
-    
-    col = [column[0] for column in cursor.description]
-
-
-    df_tkt_hist_lines=pd.DataFrame(data,columns=col)
-
-    return df_tkt_hist_lines
-
-
-def quote_import(cursor):
+def quote_import():
     #merge data from ticket and lines
 
     col=[]
@@ -133,38 +41,73 @@ def quote_import(cursor):
 
 
     df_tkt_data=pd.DataFrame(data,columns=col)
-    quote_data=pd.DataFrame(columns=['Order Reference','Customer*','Order Date','Expiration','Order Lines/Products*','Order Lines/Quantity','Order Lines/Unit Price'])
+    quote_data=pd.DataFrame(columns=['Order Reference','Customer','Order Date','Expiration','Products','Quantity','Unit Price'])
     
-    indicator=0
-    for row in  df_tkt_data.itertuples(index=False):
-        if indicator==0:
-            if(row.LINS!=0):
-                indicator=int(row.LINS)
-                #write all line information into new df at this point
-                quote_data.loc[len(quote_data)]={
-                    'Order Reference': 'S'+str(row.TKT_NO),
-                    'Customer*': row.CUST_NO,
-                    'Order Date':row.TKT_DT,
-                    'Order Lines/Products*': row.ITEM_NO,
-                    'Order Lines/Quantity': row.QTY_SOLD,
-                    'Order Lines/Unit Price': row.PRC_1
-                }
-                indicator-=1
+    quote_rows = []
+    indicator = 0
+
+    for row in df_tkt_data.itertuples(index=False):
+        if indicator == 0:#if there are no more lines on a ticket go to the next one
+            if row.LINS != 0:
+                indicator = int(row.LINS)
+                quote_rows.append({
+                    'Order_Reference': 'S' + str(row.TKT_NO),
+                    'Customer': row.CUST_NO,
+                    'Date': row.TKT_DT,
+                    'Products': row.ITEM_NO,
+                    'Quantity': row.QTY_SOLD,
+                    'Unit Price': row.PRC_1
+                })
+                indicator -= 1
         else:
-            quote_data.loc[len(quote_data)]={
-                'Order Reference': None,
-                'Customer*': None,
-                'Order Date':None,
-                'Order Lines/Products*': row.ITEM_NO,
-                'Order Lines/Quantity': row.QTY_SOLD,
-                'Order Lines/Unit Price': row.PRC_1
-            }
-            indicator-=1
+            quote_rows.append({
+                        'Order_Reference': None,
+                        'Customer': None,
+                        'Order Date': None,
+                        'Products': row.ITEM_NO,
+                        'Quantity': row.QTY_SOLD,
+                        'Unit Price': row.PRC_1
+                        })
+            indicator -= 1
 
-        print(indicator)
-
+# Create DataFrame once at the end
+    quote_data = pd.DataFrame(quote_rows) 
 
     return quote_data
+
+
+# def item_import(cursor):
+
+#     col=['External ID','Name','Product_Type','Internal_Reference','Barcode',	'Sales_Price','Cost','Weight','Sales_Description']
+#     data=[]
+
+#     cursor.execute("""SELECT ITEM_NO, DESCR, ITEM_TYP, ITEM_NO,BARCOD,REG_PRC,LST_COST,WEIGHT,LONG_DESCR
+#                       FROM IM_ITEM
+#                 """)
+    
+#     rows=cursor.fetchall()#getting all rows from table
+   
+#     for row in rows: 
+#         data.append(list(row))
+
+
+#     df_item_data=pd.DataFrame(data,columns=col)
+
+#     for data in df_item_data.itertuples(index=False):
+
+#         df_item_data.loc[
+#                 df_item_data['Product_Type'].isin(['D', 'N', 'T']),
+#                 'Product_Type'
+#             ] = 'Good'
+
+#         df_item_data.loc[
+#             ~df_item_data['Product_Type'].isin(['D', 'N', 'T']),
+#             'Product_Type'
+#             ] = 'Service'
+
+
+#     return df_item_data
+
 
 if __name__=='__main__':
     main()
